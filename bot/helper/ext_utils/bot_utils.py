b11931f4254f68170034d62d61d6359ce56fb76f
karenkaez <97656516+karenkaez@users.noreply.github.com>
2022-01-22 02:20:11 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -30,7 +30,7 @@ class MirrorStatus:
     STATUS_EXTRACTING = "Extracting...📂"
     STATUS_SPLITTING = "Splitting...✂️"
     STATUS_CHECKING = "CheckingUp...📝"
-    STATUS_SEEDING = "💐Seeding...🌩"
+    STATUS_SEEDING = " 💐X-Seeding...🌩"
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
@@ -152,13 +152,13 @@ def get_readable_message():
                            f" | <b>Leechers🥀 :</b> {download.torrent_info().num_leechs}"
                 except:
                     pass
-                msg += f"\n<code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                msg += f"\n<b> To Cancel 👉 🤦‍♀️ : </b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
             elif download.status() == MirrorStatus.STATUS_SEEDING:
                 msg += f"\n<b> Size 🌎 : </b>{download.size()}"
                 msg += f"\n<b> Speed ⚡ : </b>{get_readable_file_size(download.torrent_info().upspeed)}/s"
-                msg += f" | <b> Uploaded ☯ : </b>{get_readable_file_size(download.torrent_info().uploaded)}"
+                msg += f" | <b>Uploaded ☯ : </b>{get_readable_file_size(download.torrent_info().uploaded)}"
                 msg += f"\n<b> Ratio 🧭 : </b>{round(download.torrent_info().ratio, 3)}"
-                msg += f" | <b> Time 💐: </b>{get_readable_time(download.torrent_info().seeding_time)}"
+                msg += f" | <b> Time 🛠: </b>{get_readable_time(download.torrent_info().seeding_time)}"
                 msg += f"\n<b> To Cancel 👉 🤦‍♀️ : </b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
             else:
                 msg += f"\n<b>Size: </b>{download.size()}"
